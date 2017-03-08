4a6ca1821c2bf436d959e0bbb1532da2be51199c
amarrathore <bcs.amar1@gmail.com>
2017-03-08 17:03:18 +0530

    Deleted log4j xml

--- Volaris/src/utilities/ScreenShot.java
+++ Volaris/src/utilities/ScreenShot.java
@@ -10,18 +10,19 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class ScreenShot{
+	
+	/*
+	 * This method is used for taking screenshot.
+	 */
 	public static void TakeScreenShot(WebDriver driver, String location){
 		
 		String timeStamp=new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss").format(new Date())+".jpg";
 		File sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE); 
-		try{
-			
-			/*
-			 * Save file into specified location.
-			 */
+		try{			
+			//Save file into specified location.			 
 			FileUtils.copyFile(sourceFile, new File(location+timeStamp));
 			System.out.println("Screenshot captured successfully");
-		} 
+		}
 		catch (IOException e){
 			e.printStackTrace();
 			System.out.println("error saving screenshot file");
