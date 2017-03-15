7724cfd59690b01567550cb04add57fde586b1c8
amarrathore <bcs.amar1@gmail.com>
2017-03-15 12:32:44 +0530

    Explicit wait(locators) file updated.

--- Volaris/src/utilities/ExplicitWait.java
+++ Volaris/src/utilities/ExplicitWait.java
@@ -6,11 +6,36 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExplicitWait {
-	public static void explicitWait(WebDriver driver, String locatorId, int timeUnit)
-	{
+	public static void explicitWait_id(WebDriver driver, String locatorId, int timeUnit) {
 		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
 		explicitWait.until(ExpectedConditions.elementToBeClickable(By.id(locatorId)));
-		//explicitWait.until(ExpectedConditions.presenceOfElementLocated(By.id(locatorId)));
 	}
-
+	public static void explicitWait_className(WebDriver driver, String locatorId, int timeUnit) {
+		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.className(locatorId)));
+	}
+	public static void explicitWait_name(WebDriver driver, String locatorId, int timeUnit) {
+		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.name(locatorId)));
+	}
+	public static void explicitWait_linkText(WebDriver driver, String locatorId, int timeUnit) {
+		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.linkText(locatorId)));
+	}
+	public static void explicitWait_tagName(WebDriver driver, String locatorId, int timeUnit) {
+		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.tagName(locatorId)));
+	}
+	public static void explicitWait_partialLinkText(WebDriver driver, String locatorId, int timeUnit) {
+		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText(locatorId)));
+	}
+	public static void explicitWait_cssSelector(WebDriver driver, String locatorId, int timeUnit) {
+		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(locatorId)));
+	}
+	public static void explicitWait_xPath(WebDriver driver, String locatorId, int timeUnit) {
+		WebDriverWait explicitWait=new WebDriverWait(driver, timeUnit);
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath(locatorId)));
+	}
 }
