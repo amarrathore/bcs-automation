4bb061a14d35357b00d86820c5bd0d32c80434b2
amarrathore <bcs.amar1@gmail.com>
2017-03-16 17:03:19 +0530

    updated Files uploaded

--- Volaris/src/utilities/ConfigFileReader.java
+++ Volaris/src/utilities/ConfigFileReader.java
@@ -1,26 +1,34 @@
 package utilities;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.testng.annotations.Test;
+/**
+ * @author BCS Technology
+ * 
+ */
 public class ConfigFileReader {
+	public static File file;
 	public static FileInputStream inputStream;
-	public static Properties property;
-	
+	public static Properties pro;
+	@Test
 	// This method will return value from config properties file.
-	public static String readConfigFile(String propertyHeader)
-	{
+	public static void readConfigFile() {
 		try {
+			// Creating file object to connect config file.
+			file=new File(System.getProperty("user.dir") + "\\src\\config.properties");
 			// Creating InputStream object to read data.
-			inputStream=new FileInputStream(System.getProperty("user.dir") + "config.properties");
+			inputStream=new FileInputStream(file);
 			//Create Object of Properties Class.
-			property=new Properties();
+			pro=new Properties();
 			// Pass object reference inputStream to load method of Properties object.
-			property.load(inputStream);
+			pro.load(inputStream);			
+			inputStream.close();
 		} catch (Exception e) {
 			System.out.println("Config file couldn't be found");
 			System.out.println(e.getMessage());
 		}
-		return property.getProperty(propertyHeader);
 	}
 }
