f127f1a7ca49fe9b960153eb0c596d88e4077a81
amarrathore <bcs.amar1@gmail.com>
2017-03-20 12:11:40 +0530

    Files updated

--- Volaris/src/utilities/CrossBrowsers.java
+++ Volaris/src/utilities/CrossBrowsers.java
@@ -5,32 +5,51 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.Parameters;
+
 /**
  * @author BCS Technology
  * 
  */
 public class CrossBrowsers {
-	public static WebDriver driver;
-	// Here this parameters we will take from testng.xml
-	//@Parameters ("browser")
-	// Passing browser parameter from TestNG xml.
+	static WebDriver driver;
 	@Test
-	public static void StartBrowser(String Browser) {
-		// If the browser is Firefox, then do this
-		if(Browser.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
-			driver = new FirefoxDriver();
-		}// If browser is googleChrome, then do this
-		else if (Browser.equalsIgnoreCase("googleChrome")) {
-			System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-			driver = new ChromeDriver();
-		}// If browser is IE, then do this
-		else if (Browser.equalsIgnoreCase("IE")) {
-			System.setProperty("webdriver.ie.driver", "./Driver/IEDriverServer.exe");
-			driver = new InternetExplorerDriver();
-		}
+	// Passing browser parameter from TestNG xml.	
+	@Parameters("browser")
+	public static void StartBrowser(String browser) throws Exception {	
+			try {
+				// If the browser is Firefox, then do this
+				if(browser.equalsIgnoreCase("firefox")) {
+					System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
+					driver = new FirefoxDriver();					
+				}
+				// If browser is googleChrome, then do this
+				else if (browser.equalsIgnoreCase("googleChrome")) {
+					System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+					driver = new ChromeDriver();
+					driver.manage().window().maximize();
+				}
+				// If browser is IE, then do this
+				else if (browser.equalsIgnoreCase("IE")) {
+					System.setProperty("webdriver.ie.driver", "./Driver/IEDriverServer.exe");
+					driver = new InternetExplorerDriver();
+				}
+				else{
+				// If no browser passed throw exception
+					throw new Exception("Browser is not correct");
+				}
+				driver.get(ConfigFileReader.getLocator("URL"));
+			} catch (WebDriverException e) {
+				e.printStackTrace();
+			}
+			driver .get(ConfigFileReader.getLocator("URL"));
+	}
+
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
 	}
 }
--- Volaris/src/utilities/GenericBrowsers.java
+++ Volaris/src/utilities/GenericBrowsers.java
@@ -5,34 +5,47 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+
 /**
  * @author BCS Technology
  * 
  */
-public class GenericBrowsers {
-	//@Parameters ("browser")
+public class GenericBrowsers {	
 	static WebDriver driver;
 	// Passing browser parameter from TestNG xml.
-	public static WebDriver StartBrowser(String webURL){
-		String Browser = ConfigFileReader.getLocator("googleBrowser");
+	public static WebDriver StartBrowser(String webURL) throws Exception {
+		String browser = ConfigFileReader.getLocator("firefoxBrowser");
 		try {
 			// If the browser is Firefox, then do this
-			if(Browser.equalsIgnoreCase("firefox")) {
+			if(browser.equalsIgnoreCase("firefox")) {
 				System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
 				driver = new FirefoxDriver();
-			}// If browser is googleChrome, then do this
-			else if (Browser.equalsIgnoreCase("googleChrome")) {
+			}
+			// If browser is googleChrome, then do this
+			else if (browser.equalsIgnoreCase("googleChrome")) {
 				System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 				driver = new ChromeDriver();
-			}// If browser is IE, then do this
-			else if (Browser.equalsIgnoreCase("IE")) {
+				driver.manage().window().maximize();
+			}
+			// If browser is IE, then do this
+			else if (browser.equalsIgnoreCase("IE")) {
 				System.setProperty("webdriver.ie.driver", "./Driver/IEDriverServer.exe");
 				driver = new InternetExplorerDriver();
-			}//lauch the Website
-			driver.get(webURL);			
+			}
+			else{
+			// If no browser passed throw exception
+				throw new Exception("Browser is not correct");
+			}
+			// lauch the Website
+			driver.get(webURL);
 		} catch (WebDriverException e) {
 			e.printStackTrace();
 		}
 		return driver;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		GenericBrowsers.StartBrowser(ConfigFileReader.getLocator("URL"));
+		driver.close();
 	}
 }
